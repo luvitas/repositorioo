dfb216f7e9db8bd54064850042beda2b9340395b
luvitas <samaraaraujomaciel@gmail.com>
2024-04-08 10:56:33 -0300

    jajaa

--- pastorroberto.py
+++ pastorroberto.py
@@ -1,6 +1,9 @@
-print('pare de ser capitalista, chato, etc.')
-# kkkkkkkk, véio chato
-#kkkkkkkkkk
-#         'mateus é um genio!!')
-#aninha é maravilhosa e inteligente e AISTEN
-# eu amo isadora rodrigues.
+class Aluno:
+    def __init__(self,matricula, nome, idade,sexo):
+         self.matricula = matricula
+         self.nome=nome
+         self.idade=idade
+         self.sexo=sexo
+if __name__ == "__main__" :
+     aluno1 = Aluno("0001234", "Miguel","17","masculino") 
+     print(aluno1.matricula,aluno1.nome,aluno1.idade.aluno1.sexo)  
