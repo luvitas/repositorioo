f47fc388eb3b6e6ca8a79af526ec8f8304900f1b
luvitas <samaraaraujomaciel@gmail.com>
2024-04-08 11:06:03 -0300

    oi

--- pastorroberto.py
+++ pastorroberto.py
@@ -4,6 +4,7 @@ class Aluno:
          self.nome=nome
          self.idade=idade
          self.sexo=sexo
+
 if __name__ == "__main__" :
      aluno1 = Aluno("0001234", "Miguel","17","masculino") 
      print(aluno1.matricula,aluno1.nome,aluno1.idade.aluno1.sexo)  
