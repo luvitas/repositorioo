f2df62d0e236fc8d0887dc51fb98e9b702753661
luvitas <samaraaraujomaciel@gmail.com>
2024-04-08 11:17:27 -0300

    oioi

---
+++ samara.py
@@ -0,0 +1,8 @@
+def __init__(self,matricula, nome, idade,sexo):
+         auto . nome = nome
+         auto . idade = idade
+         auto . sexo = sexo
+
+se  __nome__  ==  "__main__" :
+     aluno1  =  Aluno ( "0001234" , "Miguel" , "17" , "masculino" )
+     imprimir ( aluno1 . matrícula , aluno1 . nome , aluno1 . idade . aluno1 . sexo )  
